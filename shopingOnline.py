b29f32b57958bf172ee6af7279f92e1b4457f39b
xiaoyutianL <1502192935@qq.com>
2024-05-18 12:49:45 +0800

    增加：异常处理、资源管理、硬编码优化、时间比较优化、代码结构优化。

--- shopingOnline.py
+++ shopingOnline.py
@@ -1,68 +1,64 @@
 import datetime
 import time
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
-# 加载所需的库
+# 优化1：将配置信息外部化，提高代码的灵活性
+browser_type = "edge"
+target_url = "https://www.jd.com"
+cart_url = "https://cart.jd.com/cart_index"
+checkout_text = "去结算"
+submit_order_text = "提交订单"
 
-now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-mstime = "2023-03-24 21:00:00.000000"
-# 首先我们需要设置抢购的时间，格式要按照预设的格式改就可以了，个月数的一定在前面加上0，例如 “01”
-
-WebBrowser = webdriver.Edge()
-# 选择使用的浏览器，如果没有Chrome浏览器可以更改其他浏览器
-
-# 淘宝 WebBrowser.get("https://www.taobao.com")
-WebBrowser.get("https://www.jd.com")
-time.sleep(3)
-# 获取网站
-
-# WebBrowser.find_element("link text", "亲，请登录").click()
-WebBrowser.find_element("link text","你好，请登录").click()
-print(f"请扫码登录")
-time.sleep(10)
-# 进入网站后读取登录链接，并扫码登录
-
-# 淘宝 WebBrowser.get("https://cart.taobao.com/cart.htm")
-WebBrowser.get("https://cart.jd.com/cart_index")
-time.sleep(3)
-# 登录后直接转跳到购物车页面
-
-while True:
+# 优化2：使用更精细的异常处理
+def safe_find_element(by, value):
     try:
-        #   if WebBrowser.find_element("id", "J_SelectAll1"):
-        #   WebBrowser.find_element("id", "J_SelectAll1").click()
-        if WebBrowser.find_element("id", "cart-body"):
-            WebBrowser.find_element("id", "cart-body").click()
+        return WebBrowser.find_element(by, value)
+    except NoSuchElementException:
+        print(f"找不到 {by} = {value} 的元素")
+        return None
+
+# 优化3：使用with语句管理资源
+with webdriver.Edge() as WebBrowser:
+    WebBrowser.get(target_url)
+    time.sleep(3)
+
+    # 登录操作（示例，需根据实际情况调整）
+    WebBrowser.find_element("link text","你好，请登录").click()
+    time.sleep(10)
+
+    WebBrowser.get(cart_url)
+    time.sleep(3)
+
+    # 优化4：减少嵌套的无限循环，简化逻辑
+    while True:
+        cart_body_element = safe_find_element("id", "cart-body")
+        if cart_body_element:
+            cart_body_element.click()
             break
-            # 这里代码意思是找到购物车全选的按钮并点击全选
-    except:
-        print(f"找不到购买按钮")
-while True:
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    print(now)
-    # print(now) 可以将实时的时间输出出来
-    if now > mstime:
-        # 当当前时间超过了抢购时间就立刻执行下面代码
-        while True:
-            try:
-                #if WebBrowser.find_element("link text", "结 算"):
-                    # WebBrowser.find_element("link text", "结 算").click()
-                if WebBrowser.find_element("link text", "去结算"):
-                    WebBrowser.find_element("link text", "去结算").click()
-                    print(f"结算成功")
-                    break
-                    # 识别界面中的“结算”按钮并点击
-            except:
-                pass
-        while True:
-            try:
-                #if WebBrowser.find_element("link text", '提交订单'):
-                    #WebBrowser.find_element("link text", '提交订单').click()
-                if WebBrowser.find_element("class name", "checkout-submit"):
-                    WebBrowser.find_element("class name", "checkout-submit").click()
-                    print(f"抢购成功，请尽快付款")
-                    # 和上面同理，识别界面中的“提交订单”按钮
-            except:
-                print(f"结算提交成功,已抢到商品啦,请及时支付订单")
-                break
-        time.sleep(0.01)
+
+    # 实时时间比较优化
+    now = datetime.datetime.now()
+    mstime = datetime.datetime.strptime("2023-03-24 21:00:00.000000", '%Y-%m-%d %H:%M:%S.%f')
+
+    while now < mstime:
+        now = datetime.datetime.now()
+        print(now.strftime('%Y-%m-%d %H:%M:%S.%f'))
+
+    # 结算操作
+    while True:
+        checkout_button = safe_find_element("link text", checkout_text)
+        if checkout_button:
+            checkout_button.click()
+            print(f"结算成功")
+            break
+
+    # 提交订单操作
+    while True:
+        submit_order_button = safe_find_element("class name", "checkout-submit")
+        if submit_order_button:
+            submit_order_button.click()
+            print(f"抢购成功，请尽快付款")
+            break
+
+    # 优化5：不再需要无限循环来保持程序运行
